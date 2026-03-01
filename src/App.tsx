d6bff33e7e9e01dd320c0d2b4b9abadd2fe71483
Isabela Targino <isabela.targino2505@icloud.com>
2026-03-01 17:18:41 -0300

    feat: initial Planejae setup w/ React and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-100">
+      <h1 className="text-4xl font-bold text-purple-700">Planejaê</h1>
+    </div>
+  );
+}
